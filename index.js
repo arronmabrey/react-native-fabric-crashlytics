2c23d042142c4e50445ea2051234682f0a52178f
Dotan Nahum <jondotan@gmail.com>
2016-07-11 20:44:18 +0300

    addressing https://github.com/mikelambert/react-native-fabric-crashlytics/issues/1

--- index.js
+++ index.js
@@ -2,21 +2,43 @@
 
 import StackTrace from 'stacktrace-js';
 import { Crashlytics } from 'react-native-fabric';
+import SourceMap from 'source-map';
 
-function init() {
+function nomap(row){
+  return {};
+}
+
+function init(smap) {
   if (__DEV__) {
     // Don't send exceptions from __DEV__, it's way too noisy!
     // Live reloading and hot reloading in particular lead to tons of noise...
     return;
   }
 
+  let mapper = nomap;
+  if(smap){
+    const mapConsumer = new SourceMap.SourceMapConsumer(smap);
+    mapper = (row)=>{
+      const loc = mapConsumer.originalPositionFor({
+                                  line: row.lineNumber,
+                                  column: row.columnNumber,
+                              });
+      return loc;
+    }
+  }
+
   var originalHandler = global.ErrorUtils.getGlobalHandler();
   function errorHandler(e, isFatal) {
     StackTrace.fromError(e, {offline: true}).then((x) => {
-      Crashlytics.recordCustomExceptionName(e.message, e.message, x.map((row) => ({
-        ...row,
-        fileName: `${row.fileName}:${row.lineNumber || 0}:${row.columnNumber || 0}`,
-      })))
+      Crashlytics.recordCustomExceptionName(e.message, e.message, x.map((row) => {
+        const loc = mapper(row);
+        return {
+          fileName: loc.source || row.fileName,
+          columnNumber: loc.column || row.columnNumber,
+          lineNumber: loc.line || row.lineNumber,
+          functionName: loc.source ? `${loc.name}@${loc.source} ${loc.line}:${loc.column}` : `${(row.source || 'unknown_func')}`, //next best thing without a consistent function name
+        };
+      }))
     });
     // And then re-throw the exception with the original handler
     if (originalHandler) {
